d9e156c37ce68d80e24a1e06a9e3c42da0e9e927
pasomateo <mberz2@pdx.edu>
2021-06-27 16:46:00 -0700

    Updated addAppointment and Collection setup

--- apptbook/src/main/java/edu/pdx/cs410J/mberz2/AppointmentBook.java
+++ apptbook/src/main/java/edu/pdx/cs410J/mberz2/AppointmentBook.java
@@ -3,18 +3,17 @@ package edu.pdx.cs410J.mberz2;
 import edu.pdx.cs410J.AbstractAppointment;
 import edu.pdx.cs410J.AbstractAppointmentBook;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Collection;
 
 public class AppointmentBook extends AbstractAppointmentBook {
 
     private String owner;
-    private Appointment appList[];
-    int tail;
+    private ArrayList<Appointment> appList;
 
     AppointmentBook(String o){
         this.owner = o;
-        this.tail = 0;
+        this.appList = new ArrayList<Appointment>();
     }
 
     @Override
@@ -24,11 +23,11 @@ public class AppointmentBook extends AbstractAppointmentBook {
 
     @Override
     public Collection getAppointments() {
-        return Arrays.asList(appList);
+        return appList;
     }
 
     @Override
     public void addAppointment(AbstractAppointment app) {
-        this.appList[tail] = (Appointment) app;
+        appList.add((Appointment) app);
     }
 }
