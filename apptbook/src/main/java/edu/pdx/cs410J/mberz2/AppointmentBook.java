0d83a5ec2f8abc0761218a37bd1b1a25b70ac0f7
pasomateo <mberz2@pdx.edu>
2021-06-27 16:16:39 -0700

    Updated getters.

--- apptbook/src/main/java/edu/pdx/cs410J/mberz2/AppointmentBook.java
+++ apptbook/src/main/java/edu/pdx/cs410J/mberz2/AppointmentBook.java
@@ -2,21 +2,33 @@ package edu.pdx.cs410J.mberz2;
 
 import edu.pdx.cs410J.AbstractAppointment;
 import edu.pdx.cs410J.AbstractAppointmentBook;
+
+import java.util.Arrays;
 import java.util.Collection;
 
 public class AppointmentBook extends AbstractAppointmentBook {
+
+    private String owner;
+    private Appointment appList[];
+    int tail;
+
+    AppointmentBook(String o){
+        this.owner = o;
+        this.tail = 0;
+    }
+
     @Override
     public String getOwnerName() {
-        return null;
+        return this.owner;
     }
 
     @Override
     public Collection getAppointments() {
-        return null;
+        return Arrays.asList(appList);
     }
 
     @Override
-    public void addAppointment(AbstractAppointment abstractAppointment) {
-
+    public void addAppointment(AbstractAppointment app) {
+        this.appList[tail] = (Appointment) app;
     }
 }
