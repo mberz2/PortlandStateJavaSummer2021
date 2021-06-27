e5339e2607f71e2f59106b7b5ec06a890e8ade28
pasomateo <mberz2@pdx.edu>
2021-06-26 18:09:47 -0700

    Create AppointmentBook.java

---
+++ apptbook/src/main/java/edu/pdx/cs410J/mberz2/AppointmentBook.java
@@ -0,0 +1,22 @@
+package edu.pdx.cs410J.mberz2;
+
+import edu.pdx.cs410J.AbstractAppointment;
+import edu.pdx.cs410J.AbstractAppointmentBook;
+import java.util.Collection;
+
+public class AppointmentBook extends AbstractAppointmentBook {
+    @Override
+    public String getOwnerName() {
+        return null;
+    }
+
+    @Override
+    public Collection getAppointments() {
+        return null;
+    }
+
+    @Override
+    public void addAppointment(AbstractAppointment abstractAppointment) {
+
+    }
+}
