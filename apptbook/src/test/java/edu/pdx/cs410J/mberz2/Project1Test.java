41eebc4a31907accb57abab21e0dbafef40927d6
pasomateo <mberz2@pdx.edu>
2021-07-01 16:27:53 -0700

    Tests for reading resources.

--- apptbook/src/test/java/edu/pdx/cs410J/mberz2/Project1Test.java
+++ apptbook/src/test/java/edu/pdx/cs410J/mberz2/Project1Test.java
@@ -1,5 +1,4 @@
 package edu.pdx.cs410J.mberz2;
-
 import org.junit.jupiter.api.Test;
 
 import java.io.BufferedReader;
@@ -7,27 +6,41 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
+
 import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
- * A unit test for code in the {@code Project 1} class.  This is different from
- * {@code Project1IT} which is an integration test (and can handle the calls
+ * A unit test for code in the {@link Project1} class.  This is different from
+ * {@link Project1IT} which is an integration test (and can handle the calls
  * to {@link System#exit(int)} and the like.
- * @see Project1
- * @see Project1IT
  */
 class Project1Test {
 
-  @Test
-  void readmeCanBeReadAsResource() throws IOException {
-    try (
-      InputStream readme = Project1.class.getResourceAsStream("README.txt")
-    ) {
-      assertThat(readme, not(nullValue()));
-      BufferedReader reader = new BufferedReader(new InputStreamReader(readme));
-      String line = reader.readLine();
-      assertThat(line, containsString("README"));
-    }
-  }
+	@Test
+	void readmeCanBeReadAsResource() throws IOException {
+		try (
+				InputStream readme = Project1.class.getResourceAsStream("README.txt")
+		) {
+			assertThat(readme, not(nullValue()));
+			BufferedReader reader = new BufferedReader(new InputStreamReader(readme));
+			String line = reader.readLine();
+			assertThat(line, containsString("README"));
+		}
+	}
+
+	@Test
+	void fileCanBePrintedAsResource() {
+		assertDoesNotThrow(()-> {
+			Project1.printRes("README.txt");
+		});
+	}
+
+	@Test
+	void invalidResourcesCannotBeRead() {
+		assertThrows(NullPointerException.class, () -> {
+			Project1.printRes("test.txt");
+		});
+	}
 }
