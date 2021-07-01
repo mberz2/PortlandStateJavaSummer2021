61cf9ae0e608f3c02754de258e08ecfaff369989
pasomateo <mberz2@pdx.edu>
2021-07-01 15:37:56 -0700

    Updated input parsing and javadocs.

--- apptbook/src/main/java/edu/pdx/cs410J/mberz2/Project1.java
+++ apptbook/src/main/java/edu/pdx/cs410J/mberz2/Project1.java
@@ -1,11 +1,6 @@
 package edu.pdx.cs410J.mberz2;
 import java.io.*;
-import java.net.URL;
-import java.nio.file.Files;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.Arrays;
-import java.util.stream.Stream;
 
 /**
  * The main class for the CS410J appointment book Project
@@ -13,47 +8,28 @@ import java.util.stream.Stream;
 public class Project1 {
 
 	public static final String USAGE =
-			"ava -jar /apptbook/target/apptbook-2021.0.0.jar [options] <args>";
-	public static final String RESOURCE =
-			"src/main/resources/edu/pdx/cs410J/mberz2/";
+			"Usage: java -jar /apptbook/target/apptbook-2021.0.0.jar " +
+					"[options] <args>";
+	public static final String README =
+			"java -jar /apptbook/target/apptbook-2021.0.0jar -README";
 
 	public static void main(String[] args) throws IOException {
 
-		boolean readmeFlag = false;
-
-		//Check number of arguments.
-		System.out.println(args.length);
-		if (args.length == 0) {
-			System.err.println("Error: Missing command line arguments");
-			printUsage();
-			System.exit(1);
-		} else if (args.length > 7) {
-			System.err.println("Error: Too many command line arguments");
-			printUsage();
-			System.exit(1);
-		}
-
-		//Check for readme flag.
-		for (String arg : args) {
-			if (arg.equalsIgnoreCase(("-README"))){
-
-				printReadme();
-
-			}
-
-		}
+		checkInput(args);
+		String [] newArgs = parseInput(args);
 	}
 
 	/**
 	 * Method to print the contents of the README.txt file as a loaded resource
 	 * from the relative-path of the resource directory. Retrieves the file
-	 * and prints it via a buffered reader.
+	 * and prints it via a buffered reader. Exits normally after the print.
 	 *
 	 * @throws IOException if unable to find the requested file.
 	 * @throws NullPointerException if the requested file is null.
 	 */
 	public static void printReadme() throws IOException {
-	  InputStream readme = Project1.class.getResourceAsStream("README.txt");
+	  InputStream readme =
+			  Project1.class.getResourceAsStream("README.txt");
 	  if (readme != null) {
 		  BufferedReader br = new BufferedReader(new InputStreamReader(readme));
 		  String line;
@@ -66,60 +42,127 @@ public class Project1 {
 			  ("No README.txt file found.");
 	}
 
-  public static String[] parseInput(String[] args){
+	/**
+	 * Method to check the correct number of arguments. The program handles the
+	 * following situations.
+	 * <ul>
+	 *     <li>0 arguments: No command line arguments, print usage.</li>
+	 *     <li>Greater than 8: The maximum number of arguments would be
+	 *     six inputs and two flags, more than 8, prints usage and exits.</li>
+	 * </ul>
+	 * @param args Array of command line arguments.
+	 */
+	public static void checkInput(String [] args){
 
-    boolean err = false;
-    SimpleDateFormat format = new SimpleDateFormat("MM-dd-yyyy");
+		//Check for correct number of arguments.
+		System.out.println(args.length);
+		if (args.length == 0) {
+			System.err.println("Error: Missing command line arguments");
+			printUsage();
+			System.exit(1);
+		} else if (args.length > 8) {
+			System.err.println("Error: Too many command line arguments");
+			printUsage();
+			System.exit(1);
+		}
+	}
 
+	/**
+	 * Method parse the command line arguments using regular expressions.
+	 * <p>First checks for options flags and ensures they are correct.</p>
+	 * <p> Next the method checks the inputs in order against the required
+	 * regular expressions.</p>
+	 * <p><b>Note:</b> If any of the expressions are incorrect, an error message
+	 * is printed displaying the mistake.</p>
+	 * <p>At the end of the parsing, if an error was detected in any arg, the
+	 * program exits with the usage message and error code.</p>
+	 *
+	 * @param args Array of command line arguments.
+	 * @return Command line arguments stripped of any usage flags.
+	 * @throws IOException if call to {@code printReadme} throws exception
+	 */
+	public static String[] parseInput(String[] args) throws IOException {
 
-    String[] newArgs = new String[0];
-    if (args.length == 7)
-      //Two options flags, take from index 1-7
-      newArgs = Arrays.copyOfRange(args, 1, 7);
+		boolean print = false;
 
-    else if (args.length == 6)
-      //One option flag, take from index 0-6
-      newArgs = Arrays.copyOfRange(args, 0, 6);
+	    //Check for readme flag.
+	    for (String arg : args) {
+	        System.out.println("1");
+		    if (arg.startsWith("-")) {
+			    if (arg.equalsIgnoreCase(("-README")))
+				    printReadme();
+			    if (arg.equalsIgnoreCase(("-PRINT")))
+				    print = true;
+			    else
+				    System.err.println(
+						    "Error: " + arg + " is not a correct option");
+		    }
+	    }
 
-    if(!(newArgs[0].matches("^[a-zA-Z\"\\s]*$"))){
-      printError("owner", newArgs[0]);
-      err = true;
+	    if (args.length == 7 && print) {
+
+		    boolean err = false;
+	    	String [] newArgs = Arrays.copyOfRange(args, 1, 7);
+	    	String alphaPat = "^[a-zA-Z\"\\s]*$";
+	    	String datePat = "^[0-3]?[0-9]/[0-3]?[0-9]/(?:[0-9]{2})?[0-9]{2}$";
+	    	String timePat = "^([01]?\\d|2[0-3]):?([0-5]\\d)$";
+
+		    if(!(newArgs[0].matches(alphaPat))){
+			    printError("owner", newArgs[0]);
+			    err = true;
+		    }
+
+		    if(!(newArgs[2].matches(datePat))){
+			    printError("begin time (date)", newArgs[2]);
+			    err = true;
+		    }
+
+		    if(!(newArgs[3].matches(timePat))){
+			    printError("begin time (time)", newArgs[3]);
+			    err = true;
+		    }
+
+		    if(!(newArgs[4].matches(datePat))){
+			    printError("end time (date)", newArgs[4]);
+			    err = true;
+		    }
+
+		    if(!(newArgs[5].matches(timePat))){
+			    printError("end time (time)", newArgs[5]);
+			    err = true;
+		    }
+
+		    if (err){
+		        System.err.println(
+		                "Run with -README to see proper formatting.");
+		        System.err.println(README);
+		        System.exit(1);
+		    }
+
+	    return newArgs;
     }
 
-    if(!(newArgs[2].matches("^[0-3]?[0-9]/[0-3]?[0-9]/(?:[0-9]{2})?[0-9]{2}$"))){
-      printError("begin time (date)", newArgs[2]);
-      err = true;
-    }
-
-    if(!(newArgs[3].matches("^([01]?\\d|2[0-3]):?([0-5]\\d)$"))){
-      printError("begin time (time)", newArgs[3]);
-      err = true;
-    }
-
-    if(!(newArgs[4].matches("^[0-3]?[0-9]/[0-3]?[0-9]/(?:[0-9]{2})?[0-9]{2}$"))){
-      printError("end time (date)", newArgs[4]);
-      err = true;
-    }
-
-    if(!(newArgs[5].matches("^([01]?\\d|2[0-3]):?([0-5]\\d)$"))){
-      printError("end time (time)", newArgs[5]);
-      err = true;
-    }
-
-    if (err)
-      printUsage();
-
-    return newArgs;
+    printUsage();
+    return null;
   }
 
-  public static void printError(String s, String x){
-    System.err.println("Error in <" + s + "> argument.");
-    System.err.println("<" + x + "> contains improper characters.");
-  }
+	/**
+	 * Method prints an error with the related argument.
+	 *
+	 * @param s String containing the incorrect field.
+	 * @param x String containing the incorrect argument.
+	 */
+	public static void printError(String s, String x){
+		System.err.println("Error in <" + s + "> argument.");
+		System.err.println("<" + x + "> contains improper characters.");
+	}
 
-  public static void printUsage(){
-    System.err.println(USAGE);
-    System.exit(1);
-  }
+	/**
+	 * Method prints the usage of the program an then exits.
+	 */
+	public static void printUsage(){
+		System.err.println(USAGE);
+		System.exit(1);
+	}
 
 }
