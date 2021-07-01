c62d7b42c2e28932a52074a073814e22bc85f6c3
pasomateo <mberz2@pdx.edu>
2021-07-01 11:07:38 -0700

    Updates + Javadocs

--- apptbook/src/main/java/edu/pdx/cs410J/mberz2/AppointmentBook.java
+++ apptbook/src/main/java/edu/pdx/cs410J/mberz2/AppointmentBook.java
@@ -1,32 +1,77 @@
 package edu.pdx.cs410J.mberz2;
-
 import edu.pdx.cs410J.AbstractAppointment;
 import edu.pdx.cs410J.AbstractAppointmentBook;
-
 import java.util.ArrayList;
 import java.util.Collection;
 
-public class AppointmentBook<T extends AbstractAppointment> extends AbstractAppointmentBook<T> {
+/**
+ * This class, an extension of AbstractAppointmentBook class, implements
+ * methods for an "appointment book" object.
+ *
+ * <p> An object is created via a parameterized constructor and private data
+ * members are accessed/retrieved via getter methods. </p>
+ *
+ * @author Matthew Berzinskas
+ * @since 2020-6-23
+ * @see AbstractAppointmentBook
+ */
+public class AppointmentBook<T extends AbstractAppointment>
+		extends AbstractAppointmentBook<T> {
 
-    private final String owner;
-    private final Collection<T> appList;
+	// Suppresses default constructor, ensuring non-instatiability.
+	private AppointmentBook(){}
 
-    AppointmentBook(String o, T app){
-        this.owner = o;
-        this.appList = new ArrayList<>();
-        appList.add(app);
-    }
+	private String owner;
+	private Collection<T> appList;
 
-    @Override
-    public String getOwnerName() {
-        return this.owner;
-    }
+	/**
+	 * Parameterized constructor for an appointment object. Sets the private
+	 * data members to the values of the passed in parameters. Initializes an
+	 * arrayList collection and adds the first appointment to the book.
+	 *
+	 * @param o String for the appointment owner.
+	 * @param app Appointment object to add to collection.
+	 * @see Appointment
+	 */
+	AppointmentBook(String o, T app){
+		this.owner = o;
+		this.appList = new ArrayList<>();
+		appList.add(app);
+	}
 
-    @Override
-    public Collection<T> getAppointments() {
-        return appList;
-    }
+	/**
+	 * Returns the appointment book's owner
+	 * @return String containing owner.
+	 * @throws UnsupportedOperationException if owner is null.
+	 */
+	@Override
+	public String getOwnerName() {
+		if (owner == null)
+			throw new UnsupportedOperationException("Book not implemented.");
+		return this.owner;
+	}
 
-    @Override
-    public void addAppointment(T app) { appList.add((T) app); }
+
+	/**
+	 * Returns the collection containing all appointments.
+	 * @return String containing owner.
+	 * @throws NullPointerException if appList is not initialized.
+	 */
+	@Override
+	public Collection<T> getAppointments() {
+		if (appList == null)
+			throw new NullPointerException("Book not implemented.");
+		return appList;
+	}
+
+	/**
+	 * Adds an appointment to the give appointment book.
+	 * @param app Appointment object to be added to the boo.
+	 * @throws NullPointerException if app to add is of Null type.
+	 * @see Appointment
+	 */
+	@Override
+	public void addAppointment(T app) {
+		appList.add((T) app);
+	}
 }
