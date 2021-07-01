a424d0d7323d42ff2f0821d6314c9fb85f6ac15d
pasomateo <mberz2@pdx.edu>
2021-07-01 10:39:14 -0700

    Added tests for beginTime, endTime, and description checking (implemented and not implemented)

--- apptbook/src/test/java/edu/pdx/cs410J/mberz2/AppointmentTest.java
+++ apptbook/src/test/java/edu/pdx/cs410J/mberz2/AppointmentTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * Unit tests for the {@link Appointment} class.
@@ -15,20 +15,50 @@ public class AppointmentTest {
 
   @Test
   void getBeginTimeStringNeedsToBeImplemented() {
-    Appointment appointment = new Appointment();
-    assertThrows(UnsupportedOperationException.class, appointment::getBeginTimeString);
+    Appointment appt =
+            new Appointment(null, null, null, null);
+    assertThrows(UnsupportedOperationException.class,
+            appt::getBeginTimeString);
+  }
+
+  @Test
+  void getBeginTimeStringImplemented() {
+    Appointment appt =
+            new Appointment(null, null, "11:30", null);
+    assertNotNull(appt.getBeginTimeString());
+    assertEquals("11:30",appt.getBeginTimeString());
+  }
+
+  @Test
+  void getEndTimeStringNeedsToBeImplemented() {
+    Appointment appt =
+            new Appointment(null, null, null, null);
+    assertThrows(UnsupportedOperationException.class,
+            appt::getEndTimeString);
+  }
+
+  @Test
+  void getEndTimeStringImplemented() {
+    Appointment appt =
+            new Appointment(null, null, null, "11:30");
+    assertNotNull(appt.getEndTimeString());
+    assertEquals("11:30",appt.getEndTimeString());
   }
 
   @Test
   void initiallyAllAppointmentsHaveTheSameDescription() {
-    Appointment appointment = new Appointment();
-    assertThat(appointment.getDescription(), containsString("not implemented"));
+    Appointment appt =
+            new Appointment(null, null, null, null);
+    assertThrows(UnsupportedOperationException.class,
+            appt::getDescription);
   }
 
   @Test
-  void forProject1ItIsOkayIfGetBeginTimeReturnsNull() {
-    Appointment appointment = new Appointment();
-    assertThat(appointment.getBeginTime(), is(nullValue()));
+  void getDescriptionStringImplemented() {
+    Appointment appt =
+            new Appointment(null, "test", null, null);
+    assertNotNull(appt.getDescription());
+    assertEquals("test",appt.getDescription());
   }
 
 }
