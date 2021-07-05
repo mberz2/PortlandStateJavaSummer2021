70ba0a6a0b8efe8d08d7c98d98ee848c26062313
pasomateo <mberz2@pdx.edu>
2021-07-05 14:38:50 -0700

    Formatting and documentation.

--- apptbook/src/main/java/edu/pdx/cs410J/mberz2/Appointment.java
+++ apptbook/src/main/java/edu/pdx/cs410J/mberz2/Appointment.java
@@ -2,8 +2,8 @@ package edu.pdx.cs410J.mberz2;
 import edu.pdx.cs410J.AbstractAppointment;
 
 /**
- * This class, an extension of {@link AbstractAppointment}, implements methods
- * for an "appointment" object.
+ * This class extends the read-only {@link AbstractAppointment} class.
+ * The class implements methods for an "appointment" object.
  *
  * <p> An object is created via a parameterized constructor and private data
  * members are accessed/retrieved via getter methods. </p>
