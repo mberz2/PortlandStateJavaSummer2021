6483f9bbd12c3bdd2e010e7d816e0ac7779921a2
pasomateo <mberz2@pdx.edu>
2021-07-07 18:17:54 -0700

    Added JavaDocs.

--- apptbook/src/main/java/edu/pdx/cs410J/mberz2/TextParser.java
+++ apptbook/src/main/java/edu/pdx/cs410J/mberz2/TextParser.java
@@ -6,17 +6,31 @@ import edu.pdx.cs410J.ParserException;
 import java.util.ArrayList;
 import java.util.Collection;
 
+/**
+ *
+ * @param <T>
+ */
 public class TextParser <T extends AbstractAppointmentBook<Appointment>>
 		implements AppointmentBookParser<T> {
 
 	private final String fileName;
 	private final Collection<T> appList;
 
+	/**
+	 *
+	 * @param fileName
+	 * @param app
+	 */
 	TextParser(String fileName, T app){
 		this.fileName = fileName;
 		this.appList = new ArrayList<>();
 	}
 
+	/**
+	 *
+	 * @return
+	 * @throws ParserException
+	 */
 	@Override
 	public T parse() throws ParserException {
 
