a4c80826dca2c38026df9662601949861fc59ca1
pasomateo <mberz2@pdx.edu>
2021-07-07 18:05:53 -0700

    Created Project2

---
+++ apptbook/src/main/java/edu/pdx/cs410J/mberz2/Project2.java
@@ -0,0 +1,26 @@
+package edu.pdx.cs410J.mberz2;
+import edu.pdx.cs410J.AbstractAppointment;
+import java.io.*;
+import java.util.Arrays;
+import java.util.Collection;
+
+/**
+ * This is the main class method for the CS410J Appointment Book Project.
+ * <p>The program simulates the receiving an appointment via command line
+ * arguments and proceeds through operations to validate the input, process
+ * a limited number of options, and then creates an {@code Appointment} and
+ * {@code AppointmentBook} object. </p>
+ * <p>If the print flag is enabled, it will print back the
+ * details of the appointment.</p>
+ * <p>If the readme flag is enabled, it will print the README.txt file that
+ * is located in the resources.</p>
+ * <p>If the textfile flag is enabled, it will read in any appointments stored
+ * in a given text file, and write the new appointment to it.</p>
+ *
+ * @author Matthew Berzinskas
+ * @since 2020-6-23
+ * @see Appointment
+ * @see AbstractAppointment
+ */
+public class Project2 {
+}
