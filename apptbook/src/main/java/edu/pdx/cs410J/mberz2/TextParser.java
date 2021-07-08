74022955764851bd6dba2729463d99eafa3aecc8
pasomateo <mberz2@pdx.edu>
2021-07-07 18:17:00 -0700

    Created TextParser layout.

--- apptbook/src/main/java/edu/pdx/cs410J/mberz2/TextParser.java
+++ apptbook/src/main/java/edu/pdx/cs410J/mberz2/TextParser.java
@@ -1,4 +1,26 @@
 package edu.pdx.cs410J.mberz2;
+import edu.pdx.cs410J.AbstractAppointmentBook;
+import edu.pdx.cs410J.AppointmentBookParser;
+import edu.pdx.cs410J.ParserException;
 
-public class TextParser {
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class TextParser <T extends AbstractAppointmentBook<Appointment>>
+		implements AppointmentBookParser<T> {
+
+	private final String fileName;
+	private final Collection<T> appList;
+
+	TextParser(String fileName, T app){
+		this.fileName = fileName;
+		this.appList = new ArrayList<>();
+	}
+
+	@Override
+	public T parse() throws ParserException {
+
+
+		return null;
+	}
 }
