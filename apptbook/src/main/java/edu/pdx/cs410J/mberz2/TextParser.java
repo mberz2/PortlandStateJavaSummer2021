2a53bd73dce3c636495dc9b1e26b9462b3865431
pasomateo <mberz2@pdx.edu>
2021-07-13 11:13:02 -0700

    Added javadocs.

--- apptbook/src/main/java/edu/pdx/cs410J/mberz2/TextParser.java
+++ apptbook/src/main/java/edu/pdx/cs410J/mberz2/TextParser.java
@@ -5,29 +5,38 @@ import java.io.*;
 import static edu.pdx.cs410J.mberz2.Project2.*;
 
 /**
+ * This class implements only class {@link AppointmentBookParser}. This class
+ * contains methods for the parsing of a BufferedReader into an appointment book
+ * object. The buffered reader contains an input stream to a File, or resource.
  *
+ *  * @author Matthew Berzinskas
+ *  * @since 2020-6-23
+ *  * @see AppointmentBook
  */
 public class TextParser implements AppointmentBookParser<AppointmentBook> {
 
+	/* Buffered reader containing the input stream. */
 	private final BufferedReader reader;
-	private String owner;
 
+	/**
+	 * Default constructor.
+	 *
+	 * @param reader Reader object containing the input stream.
+	 */
 	TextParser (Reader reader){
 		this.reader = new BufferedReader(reader);
 	}
 
-	public void setOwner(String s){
-		this.owner = s;
-	}
-
-	public boolean checkOwner(String s){
-		if (!this.owner.equals(s)){
-			System.out.println("Wrong appointment owner. Not adding.");
-			return false;
-		}
-		return true;
-	}
-
+	/**
+	 * Method for creating, parsing, and returning an AppointmentBook object
+	 * from the parser's BufferedReader data member. The method first checks
+	 * if the reader is ready, then reads line by line. If the method is unable
+	 * to validate the input against the requirements of an apptBook object, it
+	 * will exit and notify the user of a variety of errors.
+	 *
+	 * @return AppointmentBook object containing the contents of the reader.
+	 * @throws ParserException Exception handling for empty files.
+	 */
 	@Override
 	public AppointmentBook parse () throws ParserException {
 
@@ -64,21 +73,12 @@ public class TextParser implements AppointmentBookParser<AppointmentBook> {
 						parsedApp[2]+" "+parsedApp[3],
 						parsedApp[4]+" "+parsedApp[5]);
 
-				// If the owner of the parser is null, no owner has been set.
-				// Set the owner to the first arg of the first line.
-				// Otherwise, check if the owner matches, if it does, add.
-				if (owner == null) {
-					setOwner(parsedApp[0]);
-					tempBook.setOwnerName(owner);
-					tempBook.addAppointment(app);
+				tempBook.setOwnerName(parsedApp[0]);
+				tempBook.addAppointment(app);
 
-				} else if (checkOwner(parsedApp[0])) {
-					tempBook.addAppointment(app);
-				}
 			}
 
 			reader.close();
-
 			return tempBook;
 
 		} catch (IOException e) {
