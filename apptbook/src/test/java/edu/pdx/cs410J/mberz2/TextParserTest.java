314a5b1cf9dd3a43260110fbdeddac2c4fc8303d
pasomateo <mberz2@pdx.edu>
2021-07-13 18:25:33 -0700

    Testing for reading from a file and combining it with a locally created apptBook and maintaining sorted order.

--- apptbook/src/test/java/edu/pdx/cs410J/mberz2/TextParserTest.java
+++ apptbook/src/test/java/edu/pdx/cs410J/mberz2/TextParserTest.java
@@ -29,30 +29,21 @@ public class TextParserTest {
 
 		TextParser parser = new TextParser(new InputStreamReader(resource));
 		AppointmentBook first = parser.parse();
-		//System.out.println("DONE WITH FILE:\n"+first.getAppointments());
 
-		Appointment app3 = new Appointment("early", "7/11/1900 10:10 am", "7/13/1900 10:15 am");
-		Appointment app4 = new Appointment("earlier", "7/10/1900 10:10 am", "7/10/1900 10:15 am");
-		Appointment app5 = new Appointment("earliest", "7/7/1900 10:10 am", "7/7/1900 10:15 am");
+		Appointment app3 = new Appointment("early", "1/11/2021 10:10 am", "1/13/2021 10:15 am");
+		Appointment app4 = new Appointment("earlier", "12/10/2021 10:10 am", "12/10/2021 10:15 am");
+		Appointment app5 = new Appointment("earliest", "7/7/2021 10:10 am", "7/7/2021 10:15 am");
 		AppointmentBook second = new AppointmentBook("Owner", app3);
 		second.addAppointment(app4);
 		second.addAppointment(app5);
 
-		AppointmentBook newBook = new AppointmentBook();
-		newBook.setOwnerName(first.getOwnerName());
-
 		ArrayList<Appointment> appBook1 = second.getAppointments();
-		ArrayList<Appointment> appBook2 = first.getAppointments();
 
 		for (Appointment a: appBook1) {
-			newBook.addAppointment(a);
+			first.addAppointment(a);
 		}
 
-		for (Appointment a: appBook2){
-			newBook.addAppointment(a);
-		}
-
-		System.out.println(newBook.getAppointments());
+		Project3.printAppt(first);
 
 	}
 
