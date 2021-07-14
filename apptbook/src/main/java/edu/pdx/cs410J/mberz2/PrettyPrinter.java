55e2df8cf02064f08033af4a7123b00628417821
pasomateo <mberz2@pdx.edu>
2021-07-13 21:27:13 -0700

    Cleaner printing.

--- apptbook/src/main/java/edu/pdx/cs410J/mberz2/PrettyPrinter.java
+++ apptbook/src/main/java/edu/pdx/cs410J/mberz2/PrettyPrinter.java
@@ -5,6 +5,7 @@ import edu.pdx.cs410J.AppointmentBookDumper;
 
 import java.io.IOException;
 import java.io.Writer;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.concurrent.TimeUnit;
@@ -22,6 +23,8 @@ public class PrettyPrinter implements AppointmentBookDumper<AppointmentBook> {
 	public void dump(AppointmentBook appBook)
 			throws IOException {
 
+		SimpleDateFormat ft = new SimpleDateFormat("hh:mm a, E, dd MMM yyyy");
+
 		writer.write("\n===============================================================================");
 		writer.write("\n|    Appointment Book for: "+appBook.getOwnerName());
 
@@ -31,14 +34,13 @@ public class PrettyPrinter implements AppointmentBookDumper<AppointmentBook> {
 
 			for (Appointment a: apps) {
 
+				String bt = ft.format(a.getBeginTime());
+				String et = ft.format(a.getEndTime());
 
-				String[] btSplit = a.getBeginTimeString().split("\\s");
-				String[] etSplit = a.getEndTimeString().split("\\s");
 
 				writer.write("\n-------------------------------------------------------------------------------");
 				writer.write("\n| "+a.getDescription());
-				writer.write("\n|\tFrom: "+btSplit[0] + " " + btSplit[1] + " " + btSplit[2]+"\t\t"
-						+ " Until: " + etSplit[0] + " " + etSplit[1] + " " + etSplit[2]);
+				writer.write("\n| Beginning " + bt + " until "+ et);
 				writer.write("\n| Duration: "+ TimeUnit.MILLISECONDS.toMinutes(a.getEndTime().getTime() - a.getBeginTime().getTime())
 						+ " mins");
 
