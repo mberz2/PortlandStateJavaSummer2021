fe6472ef1de99b6e2b074733c70e44c06d4b427b
pasomateo <mberz2@pdx.edu>
2021-07-16 14:28:35 -0700

    Documentation.

--- apptbook/src/main/java/edu/pdx/cs410J/mberz2/PrettyPrinter.java
+++ apptbook/src/main/java/edu/pdx/cs410J/mberz2/PrettyPrinter.java
@@ -1,24 +1,45 @@
 package edu.pdx.cs410J.mberz2;
 
-import edu.pdx.cs410J.AbstractAppointmentBook;
+import edu.pdx.cs410J.AbstractAppointment;
 import edu.pdx.cs410J.AppointmentBookDumper;
 
-import java.io.IOException;
-import java.io.Writer;
+import java.io.*;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Collection;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
 
+/**
+ * This class extends the read-only {@link AppointmentBookDumper} class.
+ * The class implements methods for dumping the contents of an AppointmentBook
+ * object to a specified Writer object, set during instantiation.
+
+ * @author Matthew Berzinskas
+ * @since 2020-6-23
+ * @see AppointmentBook
+ */
 public class PrettyPrinter implements AppointmentBookDumper<AppointmentBook> {
 
 	/* Writer containing the output stream. */
 	private final Writer writer;
 
+	/**
+	 * Default constructor, sets the instances writer to the passed argument.
+	 * @param writer Argument specifying the writer stream.
+	 */
 	PrettyPrinter(Writer writer){
 		this.writer = writer;
 	}
 
+	/**
+	 * Implements a pretty writing of the passed in appointment book object to
+	 * the specified writer. This can be a file, or standard out, etc. The
+	 * method implements a more elegant date format than the default appointment
+	 * object, prints the owner, total appointments, and then each appointment
+	 * in the book. The appointment book is already sorted.
+	 *
+	 * @param appBook AppointmentBook object to print.
+	 * @throws IOException Exception handling for problems with in/out.
+	 */
 	@Override
 	public void dump(AppointmentBook appBook)
 			throws IOException {
