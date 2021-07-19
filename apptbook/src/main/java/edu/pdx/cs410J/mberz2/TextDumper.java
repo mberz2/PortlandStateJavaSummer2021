aad50ce6090137c44fe0898ee83c218540686530
pasomateo <mberz2@pdx.edu>
2021-07-19 10:44:31 -0700

    Cleaning up headers.

--- apptbook/src/main/java/edu/pdx/cs410J/mberz2/TextDumper.java
+++ apptbook/src/main/java/edu/pdx/cs410J/mberz2/TextDumper.java
@@ -3,8 +3,6 @@ package edu.pdx.cs410J.mberz2;
 import edu.pdx.cs410J.AppointmentBookDumper;
 import java.io.*;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
 
 /**
  * This class implements read-only class {@link AppointmentBookDumper}. This
