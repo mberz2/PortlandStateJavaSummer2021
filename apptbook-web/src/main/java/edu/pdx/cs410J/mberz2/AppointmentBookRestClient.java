9ce74471cbd3512a8fc2a1c5a9f348e7be200ac6
pasomateo <mberz2@pdx.edu>
2021-07-27 15:20:22 -0700

    Documentation.

--- apptbook-web/src/main/java/edu/pdx/cs410J/mberz2/AppointmentBookRestClient.java
+++ apptbook-web/src/main/java/edu/pdx/cs410J/mberz2/AppointmentBookRestClient.java
@@ -7,9 +7,15 @@ import java.io.IOException;
 import java.util.Map;
 
 /**
- * A helper class for accessing the rest client.  Note that this class provides
- * an example of how to make gets and posts to a URL.  You'll need to change it
- * to do something other than just send dictionary entries.
+ * Helper class for a the servlet. Serves as a REST client for processing the
+ * different requests from the main method. Handles GET, POST, DELETE requests
+ * through {@see HttpRequestHelper}, eventually going to
+ * {@see AppointmentBookServlet}.
+ *
+ * @author Matthew Berzinskas
+ * @since 2020-6-23
+ * @see Appointment
+ * @see AppointmentBook
  */
 public class AppointmentBookRestClient extends HttpRequestHelper
 {
@@ -18,20 +24,34 @@ public class AppointmentBookRestClient extends HttpRequestHelper
 
 	private final String url;
 
-	public AppointmentBookRestClient( String hostName, int port ) {
-		this.url = String.format( "http://%s:%d/%s/%s", hostName, port, WEB_APP, SERVLET );
+	@SuppressWarnings("DefaultLocale")
+	public AppointmentBookRestClient(String hostName, int port ) {
+		this.url = String.format( "http://%s:%d/%s/%s", hostName,
+				port, WEB_APP, SERVLET );
 	}
 
 	public Response getAllAppointments(String owner) throws IOException {
 		return get(this.url, Map.of("owner", owner));
 	}
 
-	public Response addAppointment( String owner, String description, String beginTime, String endTime) throws IOException {
-		return post(this.url, Map.of("owner", owner, "description", description, "beginTime", beginTime, "endTime", endTime));
+	public Response addAppointment( String owner,
+	                                String description,
+	                                String beginTime,
+	                                String endTime)
+			throws IOException {
+		return post(this.url, Map.of("owner", owner,
+				"description", description,
+				"beginTime", beginTime,
+				"endTime", endTime));
 	}
 
-	public Response searchTime(String owner, String beginTime, String endTime) throws IOException {
-		return get(this.url, Map.of("owner", owner, "beginTime", beginTime, "endTime", endTime));	}
+	public Response searchTime(String owner,
+	                           String beginTime,
+	                           String endTime)
+			throws IOException {
+		return get(this.url, Map.of("owner", owner,
+				"beginTime", beginTime,
+				"endTime", endTime));	}
 
 	public void deleteAllAppointments() throws IOException {
 		delete(this.url, Map.of());
