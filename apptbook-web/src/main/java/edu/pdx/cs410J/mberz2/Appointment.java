98d1b5550637ceaeac3c36a16f53f9d8a90b38e4
pasomateo <mberz2@pdx.edu>
2021-07-28 10:41:52 -0700

    Project 4 complete.

--- apptbook-web/src/main/java/edu/pdx/cs410J/mberz2/Appointment.java
+++ apptbook-web/src/main/java/edu/pdx/cs410J/mberz2/Appointment.java
@@ -40,6 +40,7 @@ public class Appointment extends AbstractAppointment
 	 * @param d String for the appointment description.
 	 * @param bt String for the appt. begin time. {@code MM/DD/YYYY HH:MM}
 	 * @param et String for the appt. end time. {@code MM/DD/YYYY HH:MM}
+	 * @throws ParserException Exception handling for parsing.
 	 */
 	Appointment (String d, String bt, String et) throws ParserException {
 		this.desc = d;
