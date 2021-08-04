a3740203d9ffc74ff4232b701694d27c12c6ed4a
pasomateo <mberz2@pdx.edu>
2021-08-04 14:28:00 -0700

    Updated help menu.

--- apptbook-android/app/src/main/java/edu/pdx/cs410j/mberz2/HelpActivity.java
+++ apptbook-android/app/src/main/java/edu/pdx/cs410j/mberz2/HelpActivity.java
@@ -6,35 +6,59 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.Spanned;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class HelpActivity extends AppCompatActivity {
+public class HelpActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
+
+        Button btnReadme = findViewById(R.id.btnReadme);
+        Button btnExit = findViewById(R.id.btnExit);
+
+        btnReadme.setOnClickListener(this);
+        btnExit.setOnClickListener(this);
+
+        String htmlAsString = getString(R.string.title_help);
+        Spanned htmlAsSpanned = Html.fromHtml(htmlAsString, Html.FROM_HTML_MODE_COMPACT);
+        TextView textView = (TextView) findViewById(R.id.titleHelp);
+        textView.setText(htmlAsSpanned);
+
+        htmlAsString = getString(R.string.version);
+        htmlAsSpanned = Html.fromHtml(htmlAsString, Html.FROM_HTML_MODE_COMPACT);
+        textView = (TextView) findViewById(R.id.titleVersion);
+        textView.setText(htmlAsSpanned);
+
+        htmlAsString = getString(R.string.created_for);
+        htmlAsSpanned = Html.fromHtml(htmlAsString, Html.FROM_HTML_MODE_COMPACT);
+        textView = (TextView) findViewById(R.id.titleFor);
+        textView.setText(htmlAsSpanned);
+
+        htmlAsString = getString(R.string.created_by);
+        htmlAsSpanned = Html.fromHtml(htmlAsString, Html.FROM_HTML_MODE_COMPACT);
+        textView = (TextView) findViewById(R.id.titleAuthor);
+        textView.setText(htmlAsSpanned);
     }
 
     @Override
-    public void onBackPressed() {
-        new AlertDialog.Builder(this)
-                .setIcon(android.R.drawable.ic_dialog_info)
-                .setTitle("Returning to Main Menu")
-                .setMessage("Are you sure you want to exit?")
-                .setPositiveButton("Yes", new DialogInterface.OnClickListener()
-                {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        finish();
-                    }
-
-                })
-                .setNegativeButton("No", null)
-                .show();
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnReadme:
+                displayReadme(v);
+                break;
+            case R.id.btnExit:
+                finish();
+        }
     }
 
     public void displayReadme(View view) {
-        startActivity(new Intent(this, ReadmeActivity.class));
+        startActivity(
+                new Intent(this, ReadmeActivity.class));
     }
 }
